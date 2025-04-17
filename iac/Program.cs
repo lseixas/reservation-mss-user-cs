972761388e235cfa31b59c68f2829b510de7865f
lseixas <lseixas.iorio@gmail.com>
2025-04-17 15:03:17 -0300

    added iac_stack to app

--- iac/Program.cs
+++ iac/Program.cs
@@ -1,12 +1,27 @@
 ﻿using Amazon.CDK;
+using iac;
+using Environment = System.Environment;
 
-namespace Iac
+namespace iac
+
 {
     public class Program
     {
         public static void Main(string[] args)
         {
             var app = new App();
+            
+            string? awsRegion = Environment.GetEnvironmentVariable("AWS_REGION");
+            string? awsAccountId = Environment.GetEnvironmentVariable("AWS_ACCOUNT_ID");
+            string? stackName = Environment.GetEnvironmentVariable("STACK_NAME");
+            string? githubRef = Environment.GetEnvironmentVariable("GITHUB_REF");
+            
+            Console.WriteLine(awsRegion);
+            Console.WriteLine(awsAccountId);
+            Console.WriteLine(stackName);
+            Console.WriteLine(githubRef);
+
+            new IacStack(app, stackName);
 
             app.Synth();
         }
