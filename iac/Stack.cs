b00d4b1becd024e5b46c7bd7d2f078f993cb46c5
lseixas <lseixas.iorio@gmail.com>
2025-04-23 14:43:11 -0300

    refactoring everything

---
+++ iac/Stack.cs
@@ -0,0 +1,37 @@
+﻿using Constructs;
+using Amazon.CDK;
+using Amazon.CDK.AWS.Lambda;
+
+namespace iac;
+
+public class Stack
+{
+    public class StackClass : Amazon.CDK.Stack
+    {
+        internal StackClass(Construct scope, string id, StackProps? props = null) : base(scope, id, props)
+        {
+            var buildOption = new BundlingOptions()
+            {
+                Image = Runtime.DOTNET_8.BundlingImage,
+                User = "root",
+                OutputType = BundlingOutput.ARCHIVED,
+                Command = new String[]
+                {
+                    "/bin/sh",
+                    "-c",
+                    "dotnet tool install -g Amazon.Lambda.Tools" +
+                    " && dotnet build" +
+                    " && dotnet lambda package --output-package /asset-output/function.zip"
+                }
+            };
+
+            var sharedLayer = new LayerVersion(this, "ReservationMssUserCsLayer", new LayerVersionProps
+            {
+                CompatibleRuntimes = new[] { Runtime.DOTNET_8 },
+                Code = Code.FromAsset("./shared/bin/Release/net8.0/publish"),
+                Description = "Lambda Layer for reservation csharp rebuild project",
+                RemovalPolicy = RemovalPolicy.DESTROY,
+            });
+        }
+    }
+}
---
+++ modules/FunctionToTest.cs
@@ -0,0 +1,18 @@
+﻿using Amazon.Lambda.Core;
+using shared;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace modules;
+
+public class FunctionToTest
+{
+    public string FunctionHandler(string input, ILambdaContext context)
+    {
+        LayerClass smtFromLayer = new LayerClass(number: 1234);
+
+        return "Hello your input was: " + input.ToUpper() + "; The number from layer is: " + smtFromLayer.Number;
+
+    }
+}
