ed82b35792ece1a9ed182b39197e3f8edbb1801f
lseixas <lseixas.iorio@gmail.com>
2025-04-23 16:12:49 -0300

    bosta

--- modules/Module1/FunctionToTest.cs
+++ modules/Module1/FunctionToTest.cs
@@ -1,4 +1,5 @@
-﻿using Amazon.Lambda.Core;
+﻿using System.Runtime.InteropServices.Marshalling;
+using Amazon.Lambda.Core;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -9,6 +10,8 @@ public class FunctionToTest
 {
     public string FunctionHandler(string input, ILambdaContext context)
     {
+
+        var smtFromLambda = new shared.LayerClass(5);
 
         return "Hello your input was: " + input.ToUpper() + "; The number from layer is: shit";
         
